77405857a372956e8355781fa6ddcd88f6a2433a
Louie Slocombe <louies@hotmail.co.uk>
2025-03-19 14:56:44 -0700

    added line 2 #1

--- template_python/__init__.py
+++ template_python/__init__.py
@@ -1,3 +1,3 @@
-from .main import print_hello, line
+from .main import print_hello, line, line_2
 
 __version__ = "0.0.0"
--- template_python/main.py
+++ template_python/main.py
@@ -1,7 +1,13 @@
 import numpy as np
 
+
 def print_hello():
     print("Hello, World!")
 
+
 def line():
     return np.linspace(0, 1, 100)
+
+
+def line_2():
+    return np.linspace(0, 2, 100)
